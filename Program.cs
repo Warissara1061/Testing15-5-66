c69cda2f779a3195022f9e9fa7b40a6875c515c1
Warissara1061 <122893696+Warissara1061@users.noreply.github.com>
2023-05-15 16:46:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace PROJECT_TEST1061{
+    class Program{
+
+        static void Main(string[] arg){
+           Console.Write("Input Town: ");
+            int T5 = 0; // จำนวนของเมือง
+            int t5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Next Step");
+
+            Console.Write("Input Number of Town: ");
+            int N5 = 0; // เลขของเมือง
+            int n5 = int.Parse(Console.ReadLine());
+
+            if(n5 == 0) {
+                Console.WriteLine("Town 1");       
+            } else {
+                Console.WriteLine("Next Town");
+            }
+            N5++;
+
+            Console.Write("Input Name of Town: ");
+            string NameTown = {0}; // ชื่อของเมือง
+            string Town1 = "Prontera";
+            string Town2 = "Geffen";
+            string Town3 = "Payon";
+            string Town4 = "Alberta";
+            string Town5 = "Al de baran";
+            string Town6 = "Yuno";
+            string Town7 = "Izude";
+            string NameTown = Console.Readline();
+            Console.WriteLine("{0}", nametown);
+
+            while(x >= 0){
+                if(x == x){
+                    Console.WriteLine("Invalid ID");
+                }
+                return x;
+            }
+            }
+            Console.WriteLine("{0}, user1");
+            Console.WriteLine("{0}, x");
+
+            Console.WriteLine("Covid-19 in this Town");
+            int c = 0; // ระดับการแพร่ระบาด
+            string spread = string.Parse(Console.ReadLine());
+            string a = string.Parse(Console.ReadLine());
+            string Accident1 = "Outbreak";
+            string Accident2 = "Vaccinate";
+            string Accident3 = "Lockdown";
+            Console.Write("Exis");
+            string E1 = "Exis";
+            string Exis = Console.Readline();
+
+            while(true) /// ask about accident
+                Console.WriteLine("To be conect? ");
+                if(S1 == spread){
+                    string S1 = "Spread";
+                    Console.WriteLine("To be conect? ");
+                    Console.Write("{0}, e1");
+                } else if (E1 == Exis){
+                    Console.WriteLine("Finish");
+                } else {
+                    Console.WriteLine("Invalid");
+                }
+                c++;
+                return a;
+
+            for ( string Accident1 = Outbreak; ++c;){
+                if(Accident2 == Vaccinate){
+                    Console.WriteLine("Spread = 0");
+                } 
+                for (string Accident3 = Lockdown; c--;){
+                    Console.WriteLine("Good bye");
+                }
+                
+            }
+
+        }
+}
